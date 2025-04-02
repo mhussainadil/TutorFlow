272a3feaf6ae04ccac92b163e2f3f4539caf3740
Mohammed Adil Hussain <mhussainadil07@gmail.com>
2025-04-03 00:42:14 +0530

    Initial commit with fixed .gitignore

--- server.js
+++ server.js
@@ -15,7 +15,14 @@ const cron = require('node-cron');
 // const io = require('socket.io')(http);
 const http = require('http');
 const { Server } = require('socket.io');
+require('dotenv').config();
 
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB Connected Successfully! '))
+.catch(err => console.error('MongoDB Connection Failed:', err));
 
 const server = http.createServer(app);
 const io = new Server(server);
